6c30cb5e08cca25b2692861544df86236fb4c91a
silsu876 <ys2110106@y-y.hs.kr>
2023-06-19 14:48:00 +0900

    Initial commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:164890824855:web:bdd3ed4da4df008b7859d2";
+  static String apiKey = "AIzaSyDcKgTkr9lBM5sj2CpT-gkwUkL9MHcCTkE";
+  static String messagingSenderId = "164890824855";
+  static String projectId = "myappproject-2368f";
+  final primaryColor = const Color(0xFFee7b64);
+}
