05d57c9f2d5de492b396bcf1ba5f438567cd90b0
silsu876 <ys2110106@y-y.hs.kr>
2023-06-20 02:18:12 +0900

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -157,106 +157,106 @@ class _HomePageState extends State<HomePage> {
         Text(
           'Index 2: School',
         ),
-        Center(
-            child: ListView(
-              padding: const EdgeInsets.symmetric(vertical: 50),
-              children: <Widget>[
-                Icon(
-                  Icons.account_circle,
-                  size: 150,
-                  color: Colors.grey[700],
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  userName,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const Divider(
-                  height: 2,
-                ),
-                ListTile(
-                  onTap: () {},
-                  selectedColor: Theme.of(context).primaryColor,
-                  selected: true,
-                  contentPadding:
-                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                  leading: const Icon(Icons.group),
-                  title: const Text(
-                    "Groups",
-                    style: TextStyle(color: Colors.black),
+          Center(
+              child: ListView(
+                padding: const EdgeInsets.symmetric(vertical: 50),
+                children: <Widget>[
+                  Icon(
+                    Icons.account_circle,
+                    size: 150,
+                    color: Colors.grey[700],
                   ),
-                ),
-                ListTile(
-                  onTap: () {
-                    nextScreenReplace(
-                        context,
-                        ProfilePage(
-                          userName: userName,
-                          email: email,
-                        ));
-                  },
-                  contentPadding:
-                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                  leading: const Icon(Icons.group),
-                  title: const Text(
-                    "Profile",
-                    style: TextStyle(color: Colors.black),
+                  const SizedBox(
+                    height: 15,
                   ),
-                ),
-                ListTile(
-                  onTap: () async {
-                    showDialog(
-                        barrierDismissible: false,
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: const Text("Logout"),
-                            content: const Text("Are you sure you want to logout?"),
-                            actions: [
-                              IconButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                icon: const Icon(
-                                  Icons.cancel,
-                                  color: Colors.red,
+                  Text(
+                    userName,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Divider(
+                    height: 2,
+                  ),
+                  ListTile(
+                    onTap: () {},
+                    selectedColor: Theme.of(context).primaryColor,
+                    selected: true,
+                    contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    leading: const Icon(Icons.group),
+                    title: const Text(
+                      "Groups",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      nextScreenReplace(
+                          context,
+                          ProfilePage(
+                            userName: userName,
+                            email: email,
+                          ));
+                    },
+                    contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    leading: const Icon(Icons.group),
+                    title: const Text(
+                      "Profile",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  ListTile(
+                    onTap: () async {
+                      showDialog(
+                          barrierDismissible: false,
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text("Logout"),
+                              content: const Text("Are you sure you want to logout?"),
+                              actions: [
+                                IconButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  icon: const Icon(
+                                    Icons.cancel,
+                                    color: Colors.red,
+                                  ),
                                 ),
-                              ),
-                              IconButton(
-                                onPressed: () async {
-                                  await authService.signOut();
-                                  Navigator.of(context).pushAndRemoveUntil(
-                                      MaterialPageRoute(
-                                          builder: (context) => const LoginPage()),
-                                          (route) => false);
-                                },
-                                icon: const Icon(
-                                  Icons.done,
-                                  color: Colors.green,
+                                IconButton(
+                                  onPressed: () async {
+                                    await authService.signOut();
+                                    Navigator.of(context).pushAndRemoveUntil(
+                                        MaterialPageRoute(
+                                            builder: (context) => const LoginPage()),
+                                            (route) => false);
+                                  },
+                                  icon: const Icon(
+                                    Icons.done,
+                                    color: Colors.green,
+                                  ),
                                 ),
-                              ),
-                            ],
-                          );
-                        });
-                  },
-                  contentPadding:
-                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                  leading: const Icon(Icons.exit_to_app),
-                  title: const Text(
-                    "Logout",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                )
+                              ],
+                            );
+                          });
+                    },
+                    contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    leading: const Icon(Icons.exit_to_app),
+                    title: const Text(
+                      "Logout",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  )
 
-              ],
-            )
-        ),
+                ],
+              )
+          ),
         ].elementAt(_selectedIndex),
       ),
       floatingActionButton: FloatingActionButton(
