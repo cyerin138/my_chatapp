0a4e25bccbfaf539f378cc0ba92d47db63d79490
정예린 <ys2110106@y-y.hs.kr>
2023-06-20 14:53:07 +0900

    color picker

--- lib/pages/drawing_page.dart
+++ lib/pages/drawing_page.dart
@@ -2,57 +2,65 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
-class DotInfo{
+class DotInfo {
   DotInfo(this.offset, this.size, this.color);
+
   final Offset offset;
   final double size;
   final Color color;
 }
 
-
-class DrawingProvider extends ChangeNotifier{
+class DrawingProvider extends ChangeNotifier {
   final lines = <List<DotInfo>>[];
 
   double _size = 3;
+
   double get size => _size;
-  set changeSize(double size){
+
+  set changeSize(double size) {
     _size = size;
     notifyListeners();
   }
 
   Color _color = Colors.black;
+
   Color get color => _color;
-  set changeColor(Color color){
+
+  set changeColor(Color color) {
     _color = color;
     notifyListeners();
   }
 
   bool _eraseMode = false;
+
   bool get eraseMode => _eraseMode;
 
-  void changeEraseMode(){
+  void changeEraseMode() {
     _eraseMode = !_eraseMode;
     notifyListeners();
   }
 
-  void drawStart(Offset offset){
+  void drawStart(Offset offset) {
     var oneLine = <DotInfo>[];
     oneLine.add(DotInfo(offset, size, color));
     lines.add(oneLine);
     notifyListeners();
   }
 
-  void drawing(Offset offset){
+  void drawing(Offset offset) {
     lines.last.add(DotInfo(offset, size, color));
     notifyListeners();
   }
 
-  void erase(Offset offset){
+  void erase(Offset offset) {
     final eraseGap = 15;
-    for(var oneLine in List<List<DotInfo>>.from(lines)){
-      for(var oneDot in oneLine){
-        if(sqrt(pow((offset.dx - oneDot.offset.dx), 2) + pow((offset.dy - oneDot.offset.dy), 2)) < eraseGap){
+    for (var oneLine in List<List<DotInfo>>.from(lines)) {
+      for (var oneDot in oneLine) {
+        if (sqrt(pow((offset.dx - oneDot.offset.dx), 2) +
+                pow((offset.dy - oneDot.offset.dy), 2)) <
+            eraseGap) {
           lines.remove(oneLine);
           break;
         }
@@ -62,53 +70,33 @@ class DrawingProvider extends ChangeNotifier{
   }
 }
 
-
 class DrawingPage extends StatefulWidget {
-
   const DrawingPage({Key? key}) : super(key: key);
 
   @override
   State<DrawingPage> createState() => _DrawingPageState();
 }
 
-
 class _DrawingPageState extends State<DrawingPage> {
+  // create some values
+  Color pickerColor = Color(0xff443a49);
+  Color currentColor = Color(0xff443a49);
+
+// ValueChanged<Color> callback
+  void changeColor(Color color) {
+    setState(() => pickerColor = color);
+  }
+
   @override
   Widget build(BuildContext context) {
     var p = Provider.of<DrawingProvider>(context);
     return Container(
       child: Column(
         children: [
-          Expanded(
-              child: Stack(
-                children: [
-                  Positioned.fill(child: CustomPaint(
-                    painter: DrawingPainter(p.lines),
-                  )),
-                  GestureDetector(
-                    behavior: HitTestBehavior.translucent,
-                    onPanStart: (s) {
-                      if(p.eraseMode){
-                        p.erase(s.localPosition);
-                      }else{
-                        p.drawStart(s.localPosition);
-                      }
-                    },
-                    onPanUpdate: (s) {
-                      if(p.eraseMode){
-                        p.erase(s.localPosition);
-                      }else{
-                        p.drawing(s.localPosition);
-                      }
-                    },
-                    child: Container(),
-                  ),
-                ],
-              )),
           Container(
             color: Colors.grey[900],
             padding:
-            EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
+                EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
@@ -117,11 +105,16 @@ class _DrawingPageState extends State<DrawingPage> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      _colorWidget(Colors.black),
-                      _colorWidget(Colors.red),
-                      _colorWidget(Colors.yellow),
-                      _colorWidget(Colors.green),
-                      _colorWidget(Colors.blue),
+                      // _colorWidget(Colors.black),
+                      // _colorWidget(Colors.red),
+                      // _colorWidget(Colors.yellow),
+                      // _colorWidget(Colors.green),
+                      // _colorWidget(Colors.blue),
+                      ElevatedButton(
+                          onPressed: () {
+                            colorDialog(context);
+                          },
+                          child: Text("ColorPicker"))
                     ],
                   ),
                 ),
@@ -165,6 +158,33 @@ class _DrawingPageState extends State<DrawingPage> {
               ],
             ),
           ),
+          Expanded(
+              child: Stack(
+            children: [
+              Positioned.fill(
+                  child: CustomPaint(
+                painter: DrawingPainter(p.lines),
+              )),
+              GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                onPanStart: (s) {
+                  if (p.eraseMode) {
+                    p.erase(s.localPosition);
+                  } else {
+                    p.drawStart(s.localPosition);
+                  }
+                },
+                onPanUpdate: (s) {
+                  if (p.eraseMode) {
+                    p.erase(s.localPosition);
+                  } else {
+                    p.drawing(s.localPosition);
+                  }
+                },
+                child: Container(),
+              ),
+            ],
+          )),
         ],
       ),
     );
@@ -189,25 +209,62 @@ class _DrawingPageState extends State<DrawingPage> {
       ),
     );
   }
+
+  colorDialog(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('ColorPicker'),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  ColorPicker(
+                    pickerColor: pickerColor,
+                    onColorChanged: changeColor,
+                  ),
+                  ElevatedButton(
+                    child: const Text('확인'),
+                    onPressed: () {
+                      setState(() => currentColor = pickerColor);
+                      _colorWidget(pickerColor);
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
 }
 
-class DrawingPainter extends CustomPainter{
+class DrawingPainter extends CustomPainter {
   DrawingPainter(this.lines);
+
   final List<List<DotInfo>> lines;
+
   @override
   void paint(Canvas canvas, Size size) {
-    for(var oneLine in lines){
+    for (var oneLine in lines) {
       Color? color;
       double? size;
       var l = <Offset>[];
       var p = Path();
-      for(var oneDot in oneLine){
+      for (var oneDot in oneLine) {
         color ??= oneDot.color;
         size ??= oneDot.size;
         l.add(oneDot.offset);
       }
       p.addPolygon(l, false);
-      canvas.drawPath(p, Paint()..color = color as Color ..strokeWidth=size as double ..strokeCap=StrokeCap.round..style=PaintingStyle.stroke);
+      canvas.drawPath(
+          p,
+          Paint()
+            ..color = color as Color
+            ..strokeWidth = size as double
+            ..strokeCap = StrokeCap.round
+            ..style = PaintingStyle.stroke);
     }
   }
 
@@ -216,6 +273,4 @@ class DrawingPainter extends CustomPainter{
     // TODO: implement shouldRepaint
     return true;
   }
-
 }
-
